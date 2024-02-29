df8f8bdf24ab5a25212c1bd7449ee6fa896f32b2
Jason <jasonolefson@gmail.com>
2024-02-29 12:58:51 -0500

    created window

---
+++ classes.py
@@ -0,0 +1,6 @@
+import pygame
+import pygwidgets
+from abc import ABC, abstractmethod
+
+# Charging Station Abstract Base Class
+
--- main.py
+++ main.py
@@ -3,3 +3,51 @@ import pygwidgets
 from classes import *
 from abc import ABC, abstractclassmethod
 
+# Create Display Window
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 800
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+timer = pygame.time.Clock() # Timer for FPS
+fps = 60
+font = pygame.font.Font(None, 36)
+
+# Game States
+main_menu = "main"
+menu_state = "main"
+
+# Classes
+
+# Helper Fucncitons
+
+# Trackers
+
+# Text
+
+# Input Text
+
+# Buttons
+exit_button = pygwidgets.TextButton(window, (290, 450), "Exit", width=200, height=50, fontSize=36)
+
+# Elements
+
+
+# Game Loop
+run = True
+
+while run:
+    window.fill(("white"))
+    timer.tick(fps)
+    
+    if menu_state == "main":
+        # Main Menu
+        exit_button.draw()
+        
+        # Event Handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if exit_button.handleEvent(event):
+                run = False
+                
+    pygame.display.update()
